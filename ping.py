eca79666334777e23ba6c4e59a3999f53712ffde
SirDuckly <77992530+SirDuckly@users.noreply.github.com>
2023-01-10 21:10:04 +0000

    Restarting mac commit

--- ping.py
+++ ping.py
@@ -2,11 +2,13 @@
 import multiprocessing
 import subprocess
 
+import time
+
 # cmd imports
 import os
 
 # Network imports
-import ipaddress
+from netaddr import IPNetwork
 
 # Goals:
 # Ping scan off given range
@@ -38,6 +40,13 @@ def check_answer(answer):
 
 def get_addr_range():
     addrRange = input("[+] Please enter the address range (example: 192.160.0.0/12 or 192.168.1.0/24): ")
+    ipAddresses= []
+    for ip in IPNetwork('192.168.0.0/16'):
+        ipAddresses.append('%s' % ip)
+    print(f'IP list = {ipAddresses}')
+    return ipAddresses
+
+    """"
     try:
         allIps=[str(ip)for ip in ipaddress.IPv4Network(addrRange)]
         if len(allIps) >= 1000:
@@ -51,25 +60,26 @@ def get_addr_range():
         return allIps
     except:
         print("Incorrect IP address range")
-    return False
+    return False"""
 
 def range_command(job_q, results_q, action):
     while True:
-        job = job_q.get()
-        if job is None:
-            break
         try:
-            if action=="ping range": # Ping IP range
-                cmdArgs = ['-c1', '-W1']
-                ping(cmdArgs, job)
-                results_q.put(job)
-            elif action=="hostname range": # Ping IP range and hostname
-                hostname = job + " : " + get_hostname(job)
-                results_q.put(hostname)
-        except:
-            print("Cmd action failed")
-            raise
-            #pass
+            job = job_q.get_nowait()
+            try:
+                if action=="ping range": # Ping IP range
+                    cmdArgs = ['-c1', '-W1']
+                    ping(cmdArgs, job)
+                    results_q.put(job)
+                elif action=="hostname range": # Ping IP range and hostname
+                    hostname = job + " : " + get_hostname(job)
+                    results_q.put(hostname)
+            except:
+                print("Cmd action failed")
+                raise
+                #pass
+        except job_q.Empty:
+            break
 
 def process_creator(action, jobResults):
     # Change from range scanner to process range creator
@@ -80,6 +90,7 @@ def process_creator(action, jobResults):
     results = multiprocessing.Queue()
     if action=="ping range":
         ipRange = get_addr_range()
+        print(f'IP RANGE: {ipRange}')
         if ipRange==False:
             return ipRange
         poolSize = len(ipRange)
@@ -111,7 +122,7 @@ def start_proc(pool):
 def set_proc_data(pingSuccess, jobs, pool, ips, pingedIps):
     if pingSuccess==False:
         for i in range(1,len(ips)):
-            print(f"Putting IP: {ips[i]}")
+            #print(f"Putting IP: {ips[i]}")
             jobs.put(ips[i])
     elif pingSuccess:
         for i in pingedIps:
